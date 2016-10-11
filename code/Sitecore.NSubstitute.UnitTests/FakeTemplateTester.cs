b0ade591b05f29364433dd6a30b1637c8da79e7d
smar <smar@sitecore.net>
2016-10-11 23:31:27 +0300

    Initial implementation of FakeTemplate has been added

---
+++ code/Sitecore.NSubstitute.UnitTests/FakeTemplateTester.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using Sitecore.Data;
+
+
+namespace Sitecore.NSubstituteUtils.UnitTests
+{
+  [TestFixture]
+  public class FakeTemplateTester
+  {
+    [Test]
+    public void FakeTemplate_ParametrlessInitialization()
+    {
+      var template = new FakeTemplate().ToSitecoreTemplate();
+
+      template.Should().NotBeNull();
+      template.ID.Should().NotBeNull();
+      template.Name.Should().Be("fakeTemplate");
+      template.BaseIDs.Should().NotBeNull();
+      template.BaseIDs.Length.Should().Be(0);
+    }
+
+    [Test]
+    public void FakeTemplate_ParametrizedInitialization()
+    {
+      ID id = ID.NewID;
+      string name = "test";
+      var template = new FakeTemplate(name, id).ToSitecoreTemplate();
+
+      template.Name.Should().Be(name);
+      template.ID.Should().Be(id);
+    }
+
+    [Test]
+    public void FakeTemplate_BaseIDs()
+    {
+      string baseIDs = ID.NewID.ToString();
+      var template = new FakeTemplate()
+        .WithBaseIDs(new ID[] {new ID(baseIDs)}).ToSitecoreTemplate();
+
+      template.BaseIDs.Length.Should().Be(1);
+      template.BaseIDs[0].Should().Be(new ID(baseIDs));
+      template.GetBaseTemplates().Should().NotBeNull();
+      template.GetBaseTemplates()[0].ID.Should().Be(new ID(baseIDs));
+    }
+  }
+}
---
+++ code/Sitecore.NSubstitute/FakeTemplate.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using Sitecore.Abstractions;
+using Sitecore.Data;
+using Sitecore.Data.Engines;
+using Sitecore.Data.Templates;
+using Sitecore.Pipelines.HttpRequest;
+
+namespace Sitecore.NSubstituteUtils
+{
+  public class FakeTemplate
+  {
+    private Template.Builder templateBuilder;
+    private TemplateEngine templateEngine;
+
+
+    public FakeTemplate(string templateName = null, ID templateId = null, Database database = null)
+    {
+      string name = string.IsNullOrEmpty(templateName) ? "fakeTemplate" : templateName;
+      ID id = templateId ?? ID.NewID;
+      Database db = database ?? FakeUtil.FakeDatabase();
+      this.Database = db;
+
+      templateEngine = Substitute.For<TemplateEngine>(db);
+      templateBuilder = new Template.Builder(name, id, templateEngine);
+      templateBuilder.SetName(name);
+      templateBuilder.SetID(id);
+
+      templateEngine.GetTemplate(id).Returns(this);
+      templateEngine.GetTemplate(name).Returns(this);
+    }
+
+    private Database Database { get; set; }
+
+    public FakeTemplate WithBaseIDs(ID[] baseIDs)
+    {
+      if (baseIDs == null || baseIDs.Length == 0)
+      {
+        return this;
+      }
+
+      StringBuilder ids = new StringBuilder();
+      for(int i = 0; i < baseIDs.Length; i++)
+      {
+        if (i > 0)
+        {
+          ids.Append("|");
+        }
+
+        ids.Append(baseIDs[i].ToString());
+        string name = "Template_" + baseIDs[i];
+        var template = new FakeTemplate(name, baseIDs[i], this.Database);
+        templateEngine.GetTemplate(baseIDs[i]).Returns(template);
+        templateEngine.GetTemplate(name).Returns(template);
+      }
+
+      templateBuilder.SetBaseIDs(ids.ToString());
+      return this;
+    }
+
+    private Template Template
+    {
+      get { return this.templateBuilder.Template; }
+    }
+
+    public static implicit operator Template(FakeTemplate fakeTemplate)
+    {
+      return fakeTemplate.Template;
+    }
+
+    public Template ToSitecoreTemplate()
+    {
+      return Template;
+    }
+  }
+}
