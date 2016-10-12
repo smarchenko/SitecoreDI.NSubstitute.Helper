328c9b22dfe2e96b603d12841ac2001b6fe20d0a
smar <smar@sitecore.net>
2016-10-12 23:27:01 +0300

    Initial implementation of fake template field has been done.

---
+++ code/Sitecore.NSubstitute.UnitTests/FakeTemplateFieldTester.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using Sitecore.Data;
+
+namespace Sitecore.NSubstituteUtils.UnitTests
+{
+  [TestFixture]
+  public class FakeTemplateFieldTester
+  {
+    [Test]
+    public void FakeTemplateField_DefaultInitialization()
+    {
+      var fakeField = new FakeTemplateField(new FakeTemplateSection(new FakeTemplate()));
+      fakeField.Builder.Should().NotBeNull();
+      var field = fakeField.ToSitecoreTemplateField();
+
+      field.Should().NotBeNull();
+      field.Name.Should().Be("fakeField");
+      field.ID.Should().NotBeNull();
+    }
+
+    [Test]
+    public void FakeTemplateField_NonDefaultInitialization()
+    {
+      string name = "test name";
+      ID id = ID.NewID;
+      var field = new FakeTemplateField(new FakeTemplateSection(new FakeTemplate()), name, id)
+        .ToSitecoreTemplateField();
+
+      field.Name.Should().Be(name);
+      field.ID.Should().Be(id);
+    }
+  }
+}
---
+++ code/Sitecore.NSubstitute/FakeTemplateField.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Data;
+using Sitecore.Data.Templates;
+
+namespace Sitecore.NSubstituteUtils
+{
+  public class FakeTemplateField
+  {
+    private TemplateField.Builder builder;
+    private FakeTemplateSection section;
+
+    public FakeTemplateField(FakeTemplateSection templateSection, string fieldName = null, ID fieldId = null)
+    {
+      var name = fieldName ?? "fakeField";
+      var id = fieldId ?? ID.NewID;
+      this.section = templateSection;
+      builder = new TemplateField.Builder(name, id, section);
+    }
+
+    public TemplateField.Builder Builder {
+      get { return builder; }
+    }
+
+    public TemplateField ToSitecoreTemplateField()
+    {
+      return builder.Field;
+    }
+
+    public static implicit operator TemplateField(FakeTemplateField field)
+    {
+      return field.ToSitecoreTemplateField();
+    }
+  }
+}
