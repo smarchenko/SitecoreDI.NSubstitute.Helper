80e99cd7c321c0e51e6c6aa5187fcdd59c9d5347
Nikolay Mitikov <mitikov@users.noreply.github.com>
2019-05-16 16:18:30 +0300

    Bug/with child does not set parent details (#16)
    * Highlight WithTemplate overrides TemplateEngine on each call
    
    * Highlight WithChild do not update child parent info
    
    * Revert test from different chapter
    
    * Enforce child item to have parent info set

--- code/Sitecore.NSubstitute.UnitTests/FakeItemTester.cs
+++ code/Sitecore.NSubstitute.UnitTests/FakeItemTester.cs
@@ -6,6 +6,7 @@ using NSubstitute;
 using Sitecore.Collections;
 using Sitecore.Data;
 using Sitecore.Data.Items;
+using Sitecore.Data.Templates;
 using Sitecore.NSubstituteUtils;
 using Sitecore.Security.AccessControl;
 using Xunit;
@@ -313,6 +314,41 @@ namespace Sitecore.NSubstitute.UnitTests
             scItem.GetChildren(ChildListOptions.None).Should().HaveCount(2);
         }
 
+        [Theory, AutoData]
+        public void WithChild_WhenCalled_SetsParentIdToChild(ID childId, ID parentId)
+        {
+            // Arrange
+            var fakeChild = new FakeItem(childId);
+
+
+            Item parent = new FakeItem(parentId)
+                // Act
+                .WithChild(fakeChild);
+
+            var childParentId = fakeChild.ToSitecoreItem().ParentID;
+
+            // Assert
+            childParentId
+                .Should().Be(parentId, because: "item was added as a child to parent");
+        }
+
+        [Theory, AutoData]
+        public void WithChild_WhenCalled_SetsParentToChild(ID childId, ID parentId)
+        {
+            // Arrange
+            var fakeChild = new FakeItem(childId);
+
+            Item parent = new FakeItem(parentId)
+                // Act
+                .WithChild(fakeChild);
+
+            var childParentItem = fakeChild.ToSitecoreItem().Parent;
+
+            // Assert
+            childParentItem
+                .Should().Be(parent, because: "item was added as a child to parent");
+        }
+
         #region WithTemplate tests
 
         [Theory, AutoData]
@@ -345,17 +381,21 @@ namespace Sitecore.NSubstitute.UnitTests
         }
 
         [Theory, AutoData]
-        public void WithTemplate_WhenCalled_ConfiguresTemplateEngineToFindItemTemplate(ID templateId)
+        public void WithTemplate_WhenCalled_ConfiguresTemplateEngineToFindItemTemplate(ID templateId, string contextDatabaseName)
         {
             // Arrange
-            Item item = new FakeItem().WithTemplate(templateId);
+            var database = FakeUtil.FakeDatabase(contextDatabaseName);
+            Item item = new FakeItem(database: database).WithTemplate(templateId);
 
             // Act
-            var itemTemplate = item.Database.Engines.TemplateEngine.GetTemplate(templateId);
+            var itemTemplate = database.Engines.TemplateEngine.GetTemplate(templateId);
 
             // Assert
-            itemTemplate.Should().NotBeNull();
+            itemTemplate
+                .Should().NotBeNull()
+                .And.Match<Template>(template => template.ID == templateId);
         }
+
         #endregion
 
         [Theory, InlineData("my test item")]
